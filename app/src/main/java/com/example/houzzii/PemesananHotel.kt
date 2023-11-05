f7a08a11a6051435f02d15a8f76ffe7f2deb4960
rismayo <rismamanura855@gmail.com>
2023-11-05 14:34:15 +0700

    membuat PemesananHotel.kt

---
+++ app/src/main/java/com/example/houzzii/PemesananHotel.kt
@@ -0,0 +1,11 @@
+package com.example.houzzii
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PemesananHotel : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pemesanan_hotel)
+    }
+}
