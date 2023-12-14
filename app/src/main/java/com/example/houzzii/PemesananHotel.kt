6e1db2c5f4578a520d0ed548e41ec49a822f3ad0
rismayo <rismamanura855@gmail.com>
2023-12-14 10:35:37 +0700

    menambahkan riwayat.kt dan profile.kt

--- app/src/main/java/com/example/houzzii/PemesananHotel.kt
+++ app/src/main/java/com/example/houzzii/PemesananHotel.kt
@@ -1,11 +1,20 @@
 package com.example.houzzii
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class PemesananHotel : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pemesanan_hotel)
+
+        var btnIntent = findViewById(R.id.PesanHotel) as Button
+        btnIntent.setOnClickListener{
+            startActivity(Intent(this, MainActivity::class.java))
+            Toast.makeText(this, "Berhasil Booking Hotel", Toast.LENGTH_SHORT).show()
+        }
     }
 }
